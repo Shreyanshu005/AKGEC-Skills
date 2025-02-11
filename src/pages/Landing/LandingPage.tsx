2b59d8167908c912dddb62fc14495203b4e90b1c
Kartikay Singh pundir <130781246+kartikay6969@users.noreply.github.com>
2025-02-11 15:29:59 +0530

    intialised project

---
+++ src/pages/Landing/LandingPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Navbar from '../../components/Navbar'
+const LandingPage = () => {
+  return (
+    <>
+      <Navbar />
+    </>
+  )
+}
+
+export default LandingPage
